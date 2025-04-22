752b223cbb80f65b8f3e428319e48b14c5f048a4
Miyn97 <tn9797@naver.com>
2025-04-22 13:31:56 +0900

    docs : 플레이어 스탯 변수 선언
    플레이어 스탯 변수 선언

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    public class Player
+    {
+        public string playerName;
+        public string job;
+        public float atk;
+        public float def;
+        public float spi; //정신력
+    }
+}
