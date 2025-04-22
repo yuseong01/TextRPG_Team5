3852860d70f84ba7802412a87e4eaab36151e185
Miyn97 <tn9797@naver.com>
2025-04-22 20:26:52 +0900

    fix : 충돌 방지를 위한 텍스트 삭제
    충돌 방지를 위한 텍스트 삭제

--- Program.cs
+++ Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Player player = new Player("수민", "학생");
-        PlayerStatUI statUI = new PlayerStatUI(); 
 
-        GameIntroUI intro = new GameIntroUI(player, statUI); // 전달
-        intro.FirstIntro();
-        intro.GameIntro(); 
-        Console.ReadLine(); // 화면 유지
     }
 }
