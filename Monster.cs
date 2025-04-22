5716b82d0b68ed32cfb5a000f86a3e5e0c19f4c8
ilvpap <85211001+ilvpap@users.noreply.github.com>
2025-04-22 20:59:48 +0900

    feat : Monster, MonsterAcitve
    몬스터, 몬스터행동 추가

---
+++ Monster.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    class Monster
+    {
+        // 속성 (attributes)
+        public string Name { get; private set; }
+        public int MaxHealth { get; private set; }
+        public int CurrentHealth { get; private set; }
+        public int AttackPower { get; private set; }
+        public int Defense { get; private set; }
+        public int ExperienceReward { get; private set; }
+
+        // 생성자 (Constructor)
+        public Monster(string name, string question, string CorrectAnswer)
+        {
+            Name = name;
+            question = question;
+            CorrectAnswer = CorrectAnswer;
+        }
+        public class NormalMonster : Monster
+        {
+            public NormalMonster(string question, string correctAnswer)
+                :base("노말 몬스터", question, correctAnswer) { }
+        }
+        public class HardMonster : Monster
+        {
+            public HardMonster(string question, string correctAnswer)
+                :base("하드 몬스터", question, correctAnswer) { }
+        }
+    }
+}
---
+++ MonsterActive.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    class MonsterActive
+    {
+        protected Monster monster;
+
+        public MonsterActive(Monster monster)
+        {
+            this.monster = monster;
+        }
+        public virtualvoid AttackPlayer()
+        {
+            Console.WriteLine($"{monster.Name}의 질문: {monster.Question}");
+            Console.Write("답변:");
+            string input = Console.ReadLine();
+
+            if (input == monster.CorrectAnswer)
+            {
+                Console.WriteLine("정답입니다! 몬스터를 물리쳤습니다.");
+            }
+            else
+            {
+                Console.WriteLine("오답임니다! 몬스터가 공격합니다!");
+            }
+        }
+        public class NormalMonsterActive : MonsterActive
+        {
+            public NormalMonsterActive(Monster monster) : base(monster) { }
+            public override void AttackPlayer()
+            {
+                base.AttackPlayer();
+                // 추가 효과가 있다면 여기 구현
+            }
+        }
+        public class HardMonsterActive : MonsterActive
+        {
+            public HardMonsterActive(Monster monster) : base(monster)
+            {
+                base.AttackPlayer();
+                if(monster.CorrectAnswer != Console.ReadLine())
+                {
+                    Console.WriteLine("정신력이 깎입니다! (하드몬 특수효과)");
+                    // vmffpdldj cpfur RkRsms zhem emd
+                }
+            }
+        }
+    }
+}
