7df1be8edcd8ec0ae04e4c56c9aaf67f6e884be5
chae_kkung <ycy0109@naver.com>
2025-04-25 02:19:41 +0900

    add : 아스키아트추가
    아스키아트를 넣었습니다.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ class Program
 {
     public static void Main(string[] args)
     {
-        GameManager gameManager = new GameManager();
-        gameManager.GameStart();
-
+        Map1 map1 = new Map1();
+        map1.Draw();
+        //GameManager gameManager = new GameManager();
+        //gameManager.GameStart();
         //UI테스트 용이라 주석처리 (지우지마시오)
         //UIManager uiManager = new UIManager();
         //StoryText story = new StoryText(uiManager);
