1995fcc34082c37627b5e17e8704244d63561f4b
chae_kkung <ycy0109@naver.com>
2025-04-26 00:42:12 +0900

    feat : 상점, 아이템 구현
    무기/방어구 아이템 추가
    상점 기능 구현

--- Model/Shop.cs
+++ Model/Shop.cs
@@ -1,47 +1,55 @@
-﻿namespace week3
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace week3
 {
     public class Shop
     {
         private List<Item> shopItems = new List<Item>();
         private InventoryManager inventory;
-        
+        private Player player;
+        int number = 0;
         public Shop(InventoryManager inventory)
         {
             this.inventory = inventory;
             // 초기 상점 아이템 구성
-            shopItems.Add(new Item("이름1", "설명1", 1100));
-            shopItems.Add(new Item("이름2", "설명2", 1200));
-            shopItems.Add(new Item("이름3", "설명3", 1300));
+            shopItems.Add(inventory.ItemData[10]);
+            shopItems.Add(inventory.ItemData[11]);
+            shopItems.Add(inventory.ItemData[12]);
+            shopItems.Add(inventory.ItemData[13]);
+            shopItems.Add(inventory.ItemData[14]);
+            shopItems.Add(inventory.ItemData[15]);
         }
-        
-        public void ShowShop()
+
+        public void ShopItemList()
         {
+            number = shopItems.Count;
             Console.WriteLine("=== 상점 목록 ===");
-            for (int i = 0; i < shopItems.Count; i++)
+            for (int i = 0; i < number; i++)
             {
                 Item item = shopItems[i];
                 Console.WriteLine($"{i + 1}. {item.Name} ({item.Price}G) - {item.Description}");
             }
+
         }
 
-        public void BuyItem(int index, ref int gold)
-        {
-            if (index < 0 || index >= shopItems.Count)
-            {
-                Console.WriteLine("존재하지 않는 아이템입니다.");
-                return; 
-            }
 
-            Item selected = shopItems[index];
-            if (gold < selected.Price)
+        public void BuyItem()
+        {
+            Console.WriteLine("구매를 원하시는 아이템의 번호를 입력해 주세요.");
+            int index = InputManager.GetInt(1, number) - 1;
+            int gold = player.Gold;
+            if (gold < shopItems[index].Price)
             {
                 Console.WriteLine("골드가 부족합니다!");
-                return;
+                ShopItemList();
             }
-
-            gold -= selected.Price;
-            inventory.AddItem(selected);
-            Console.WriteLine($"{selected.Name}을(를) 구매했습니다!");
+            else
+            {
+                inventory.AddItem(index);
+                // 플레이어 클래스에 골드를 차감하는 함수를 추가한 후 여기에 넣어주세요.(매개변수로 int gold를 넣어주세요)
+                Console.WriteLine($"{shopItems[index].Name}을(를) 구매했습니다!");
+                ShopItemList();
+            }
         }
     }
 }
