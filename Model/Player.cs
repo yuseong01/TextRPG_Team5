014fc7724c85875621ad79b185a48a1f26e9254b
chae_kkung <ycy0109@naver.com>
2025-04-26 01:08:00 +0900

    feat : player 골드 감소 함수 생성 및 상점 기능 보강

--- Model/Player.cs
+++ Model/Player.cs
@@ -12,7 +12,7 @@
         public int Defense { get; set; }
 
         public int Hp { get; set; }
-        public int Gold { get; set; }
+        public int Gold { get; private set; }
         public int ZebCoin { get; set; }
 
         public int Spirit { get; private set; }
@@ -34,21 +34,29 @@
 
 
         // 보상 시스템 추가
-        public void AddZebCoin(int amount)
+        public void AddZebCoin(int value)
         {
-            ZebCoin += amount;
-            Console.WriteLine($"[시스템] {Name}의 ZebCoin이 {amount} 증가! (현재 : {ZebCoin})");
+            ZebCoin += value;
+            Console.WriteLine($"[시스템] {Name}의 ZebCoin이 {value} 증가! (현재 : {ZebCoin})");
         }
 
-        public void AddGold(int amount)
+        public void AddGold(int value)
         {
-            Gold += amount;
-            Console.WriteLine($"[시스템] {Name}의 Gold가 {amount} 증가! (현재: {Gold})");
+            Gold += value;
+            Console.WriteLine($"[시스템] {Name}의 Gold가 {value}G 증가! (현재: {Gold})");
         }
+
+        // 골드 차감
+        public void SpendGold(int value)
+        {
+            Gold -= value;
+            Console.WriteLine($"[시스템] {Name}의 Gold가 {value}G 감소! (현재: {Gold})");
+        }
+
 
         // 아이템 보상 시스템 추가 
         // itemName CS1503 오류 (타입 불일치) 로 인한 주석처리 -> Inventory 클래스가 Item 객체를 요구하도록 설계되었으나 문자열 전달
-        public void AddItem(string itemName)
+        public void AddItem(int index)
         {
             //inventory.AddItem(itemName);
             //Console.WriteLine($"[획득] {itemName}을(를) 얻었습니다!");
