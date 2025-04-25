4ad05f9e77a572f8aeb47116612e4473d391422a
JangYuseong <dog2655@naver.com>
2025-04-25 14:53:22 +0900

    feat: MapManager에서 MapObject생성 및 GameManager에서 Map호출

--- GameManager.cs
+++ GameManager.cs
@@ -16,8 +16,17 @@ namespace week3
         public void GameStart() 
         {
             //uiManager.ShowGameIntroUI();
-            Console.ReadLine(); // 화면 유지
-            mapManager.ManageMap();
+            //Console.ReadLine(); // 화면 유지
+
+            mapManager.LoadSelectedMap(MapManager.mapType.GroupFiveMap); //0번째 맵으로 들어감(5조)
+            mapManager.LoadSelectedMap(MapManager.mapType.PassageMap); //복도
+            mapManager.LoadSelectedMap(MapManager.mapType.Manager1RoomMap); //매니저1방
+            mapManager.LoadSelectedMap(MapManager.mapType.PassageMap); //복도
+            mapManager.LoadSelectedMap(MapManager.mapType.Manager2RoomMap); //매니저2방
+            mapManager.LoadSelectedMap(MapManager.mapType.PassageMap); //복도
+            mapManager.LoadSelectedMap(MapManager.mapType.Manager3RoomMap); //매니저3방
+
+            
         }
     }
 }
--- MapManager.cs
+++ MapManager.cs
@@ -4,20 +4,15 @@ using week3;
 public class MapManager
 {
     MapUI mapUI = new MapUI();
-    MapObject[] objectList = new MapObject[4];
-    public List<MapObject> mapObjects; //맵오브젝트는 각각의 맵에 있는 사물. //5조, 매니저룸1,2,3 이렇게 총 4개 필요함
-    
+    List<MapObject> mapObjectList = new List<MapObject>();
 
     public MapManager()
     {
-        mapObjects = new List<MapObject>()
-        {
-            new MapObject("인벤토리", "주머니를 확인해보자.",  ObjectType.Inventory),
-            new MapObject("컴퓨터", "컴퓨터가 켜져있다. ZEb 상점에 접속할 수 있을 것 같다.", ObjectType.Store),
-            new MapObject("캐비넷", "안에 무언가 들어있다. 비밀번호를 입력하면 열 수 있을 것 같다.", ObjectType.Money),
-            new MapObject("서류더미", "어지러이 놓여진 서류를 들춰보니 메모지가 보인다.", ObjectType.Quiz),
-            new MapObject("쓰레기통", "열쇠를 발견했다... 음? 열쇠가 움직인다...?", ObjectType.Monster),
-        };
+        mapObjectList.Add(new MapObject("인벤토리", "주머니를 확인해보자.",  ObjectType.Inventory));
+        mapObjectList.Add(new MapObject("컴퓨터", "컴퓨터가 켜져있다. ZEb 상점에 접속할 수 있을 것 같다.", ObjectType.Store));
+        mapObjectList.Add(new MapObject("캐비넷", "안에 무언가 들어있다. 비밀번호를 입력하면 열 수 있을 것 같다.", ObjectType.Money));
+        mapObjectList.Add(new MapObject("서류더미", "어지러이 놓여진 서류를 들춰보니 메모지가 보인다.", ObjectType.Quiz));
+        mapObjectList.Add(new MapObject("쓰레기통", "열쇠를 발견했다... 음? 열쇠가 움직인다...?", ObjectType.Monster));
     }
     public void ManageMap() //ManageMap(요기 불러오는거는 나중에 gameManager에서 하는걸로 변경하기)
     {
