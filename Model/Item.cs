204039321631ca4bce6ca33209b5d94edc28fae2
chae_kkung <ycy0109@naver.com>
2025-04-25 22:34:40 +0900

    refactor : 중복 기능 스크립트 병합 및 역할 변경

--- Model/Item.cs
+++ Model/Item.cs
@@ -4,12 +4,14 @@
     {
         public string Name { get; private set; }
         public string Description { get; private set; }
+        public string Type { get; private set; }
         public int Price { get; private set; }
 
-        public Item(string name, string description, int price)
+        public Item(string name, string description, string type, int price)
         {
             Name = name;
             Description = description;
+            Type = type;
             Price = price;
         }
     }
