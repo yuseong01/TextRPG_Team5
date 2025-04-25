0438000684c99ef16be5762a81d259df2766d6d2
chae_kkung <ycy0109@naver.com>
2025-04-25 10:59:55 +0900

    feat : InputManager추가
    InputManager을 추가.
    - 숫자를 입력받는 GetInt 함수를 넣었습니다.

---
+++ InputManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    internal static class InputManager
+    {
+        public static int GetInt(int min, int max)
+        {
+            while (true)
+            {
+                Thread.Sleep(700);
+                Console.Clear();
+                Console.Write("번호를 선택해주세요. >");
+                string input = Console.ReadLine();
+                bool isNumber = int.TryParse(input, out int value);
+
+                if (isNumber)
+                {
+                    if (value >= min && value <= max)
+                    {
+                        return value;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"{min}부터 {max} 사이의 숫자를 입력해주세요.");
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("숫자를 입력해주세요.");
+                }
+            }
+        }
+
+    }
+}
