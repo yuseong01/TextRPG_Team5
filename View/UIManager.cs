cdb8d04392b41f2f65988627763d4e245b8f2f26
chae_kkung <ycy0109@naver.com>
2025-04-26 11:18:09 +0900

    refactor : 불필요한 쪼개기 병합

--- View/UIManager.cs
+++ View/UIManager.cs
@@ -68,6 +68,24 @@ namespace week3
                     break;
             }
         }
+        //보스 몬스터
+        public  void ShowBossStatus(BossMonster_Data boss)
+        {
+            Console.Clear();
+            Console.WriteLine($"<매니저 스펙>");
+            Console.WriteLine(new string('=', 35));
+            Console.WriteLine($"{boss.Name}");
+            Console.WriteLine($"공격력: {boss.Atk}");
+            Console.WriteLine($"방어력: {boss.Def}");
+            Console.WriteLine($"체력: {boss.MaxHP}");
+            Console.WriteLine($"정신력: {boss.MaxSpirit}");
+            Console.WriteLine($"치명타율: {boss.Critical}%");
+            Console.WriteLine($"회피율: {boss.Evasion}%");
+            Console.WriteLine(new string('=', 35));
+            Console.WriteLine();
+            Console.ReadKey(true);
+            BossMonster_Battle.PlayerTurn();
+        }
     }
 
 }
