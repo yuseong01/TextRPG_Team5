d495437e9818ea3c230bb3c42e20a393b9383f94
chae_kkung <ycy0109@naver.com>
2025-04-27 20:42:43 +0900

    feat : 아이템 value 나오는 기능 추가(진행중)

--- Controller/InventoryManager.cs
+++ Controller/InventoryManager.cs
@@ -20,20 +20,25 @@ namespace week3
             while (true)
             {
                 Console.Clear();
-                if(inventoryType==0)    //비전투
+                if (inventoryType == 0)    //비전투
                 {
                     for (int i = 0; i < playerItems.Count; i++)
-                        {
-                            string index = (playerItems[i].IsEquip) ? $"[E]{i + 1}." : $"{i + 1}.";
-                            Console.WriteLine($"{index} {playerItems[i].Name,-10}| {playerItems[i].Description,-30}");
-                        }
-                        Console.Write("장착할 아이템 번호를 선택해주세요. \n나가려면 0을 눌러주세요.>");
+                    {
 
-                        int inputNum = InputManager.GetInt(0, playerItems.Count);
-                        if (inputNum == 0) break;
-                        EquipItem(playerItems[inputNum-1]);
+                        if (playerItems[i].Type == "무기")
+                        {
+
+                        }
+                        string index = (playerItems[i].IsEquip) ? $"[E]{i + 1}." : $"{i + 1}.";
+                        Console.WriteLine($"{index} {playerItems[i].Name,-10}| {playerItems[i].Description,-30}");
+                    }
+                    Console.Write("장착할 아이템 번호를 선택해주세요. \n나가려면 0을 눌러주세요.>");
+
+                    int inputNum = InputManager.GetInt(0, playerItems.Count);
+                    if (inputNum == 0) break;
+                    EquipItem(playerItems[inputNum - 1]);
                 }
-                else if(inventoryType==1)   //일반전투
+                else if (inventoryType == 1)   //일반전투
                 {
                     for (int i = 0; i < nomalBattleItemList.Count; i++)
                     {
@@ -42,21 +47,21 @@ namespace week3
                     Console.Write("사용할 아이템 번호를 선택해주세요. \n나가려면 0을 눌러주세요.>");
                     int inputNum = InputManager.GetInt(0, playerItems.Count);
                     if (inputNum == 0) break;
-                    UseItem(nomalBattleItemList[inputNum-1]);
+                    UseItem(nomalBattleItemList[inputNum - 1]);
                     break;
                 }
-                else if(inventoryType==2)   //보스전투
+                else if (inventoryType == 2)   //보스전투
                 {
                     for (int i = 0; i < bossBattleItemList.Count; i++)
-                        {
-                            Console.WriteLine($"{i + 1} {bossBattleItemList[i].Name,-10}| {bossBattleItemList[i].Description,-30}");
-                        }
-                        Console.Write("사용할 아이템 번호를 선택해주세요. \n나가려면 0을 눌러주세요.>");
-                        int inputNum = InputManager.GetInt(0, playerItems.Count);
-                        if (inputNum == 0) break;
-                        bossBattleItemList[inputNum-1].IsUsed=true;
-                        UseItem(bossBattleItemList[inputNum-1]);
-                        break;
+                    {
+                        Console.WriteLine($"{i + 1} {bossBattleItemList[i].Name,-10}| {bossBattleItemList[i].Description,-30}");
+                    }
+                    Console.Write("사용할 아이템 번호를 선택해주세요. \n나가려면 0을 눌러주세요.>");
+                    int inputNum = InputManager.GetInt(0, playerItems.Count);
+                    if (inputNum == 0) break;
+                    bossBattleItemList[inputNum - 1].IsUsed = true;
+                    UseItem(bossBattleItemList[inputNum - 1]);
+                    break;
                 }
             }
         }
@@ -77,12 +82,12 @@ namespace week3
 
         void UseItem(Item item)
         {
-            if(item.Type == "회복")
+            if (item.Type == "회복")
             {
                 nomalBattleItemList.Remove(item);
                 Console.WriteLine($"{item.Name}을(를) 사용했습니다.");
             }
-            else if(item.Type=="보스")
+            else if (item.Type == "보스")
             {
                 bossBattleItemList.Remove(item);
                 //아이템 사용 여부 bool값을 바꿔?주기.
